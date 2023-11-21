f6ad8562d6301a6eb0d497b200eb6000cb1bbc3b
juuf <47811956+juuf@users.noreply.github.com>
2023-11-21 09:57:53 +0100

    Docker->Podman in README

--- webserver/database.php
+++ webserver/database.php
@@ -8,16 +8,15 @@
 global $log_all_errors;
 $log_all_errors = FALSE;
 
+
 //********************************************************************************************************************
 // MYSQL DATABASE CONNECTION:
 // Replace the values inside the single quotes below with the values for your MySQL configuration. 
 // If not using the default port 3306, then append a colon and port number to the hostname (e.g. $hostname = 'example.com:3307';).
-$hostname 	= $_ENV['PMA_HOST']
-$db 		= $_ENV['MYSQL_DATABASE']
-$username 	= $_ENV['PMA_RCUSER']
-$password 	= $_ENV['MYSQL_ROOT_PASSWORD']
-
-// $_ENV['REDCAP_SALT']
+$hostname 	= $_ENV['PMA_HOST'];
+$db 		= $_ENV['MYSQL_DATABASE'];
+$username 	= $_ENV['PMA_RCUSER'];
+$password 	= $_ENV['MYSQL_ROOT_PASSWORD'];
 
 // You may optionally utilize a database connection over SSL/TLS for improved security. To do so, at minimum
 // you must provide the path of the key file, the certificate file, and certificate authority file.
@@ -39,9 +38,7 @@ $db_ssl_verify_server_cert = false; // Set to TRUE to force the database connect
 // SALT VARIABLE:
 // Add a random value for the $salt variable below, preferably alpha-numeric with 8 characters or more. This value wll be 
 // used for data de-identification hashing for data exports. Do NOT change this value once it has been initially set.
-
-$salt = 'qwertyui';
-
+$salt=$_ENV['REDCAP_SALT'];
 
 //********************************************************************************************************************
 // DATA TRANSFER SERVICES (DTS):
