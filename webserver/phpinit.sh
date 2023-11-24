9dc3a0c8f36eb5527151bfb45e1210fa763bff64
juuf <47811956+juuf@users.noreply.github.com>
2023-11-24 16:36:51 +0100

    () in PhP not working anymore after use of php.ini

--- webserver/phpinit.sh
+++ webserver/phpinit.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+export variables_order="\x22EGPCS\x22"
 export memory_limit=4G
 export max_execution_time=800
 export post_max_size=8G
 export upload_max_filesize=8G
 export max_input_vars=100000
 
-for key in upload_max_filesize post_max_size max_execution_time max_input_vars memory_limit
-do
- sed -i "s/^\($key\).*/\1 $(eval echo = \${$key})/" /usr/local/etc/php/php.ini
+for key in variables_order upload_max_filesize post_max_size max_execution_time max_input_vars memory_limit
+do 
  sed -i "s/^;\($key\).*/\1 $(eval echo = \${$key})/" /usr/local/etc/php/php.ini
+ sed -i "s/^\($key\).*/\1 $(eval echo = \${$key})/" /usr/local/etc/php/php.ini
 done
